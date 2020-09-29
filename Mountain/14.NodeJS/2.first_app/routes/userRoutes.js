137f8dce094b785b9aca81dd7e7a7d7665f6d6f2
dewerseb <bastien.dewerse@gmail.com>
2020-09-29 14:11:27 +0200

    Node first app done

--- Mountain/14.NodeJS/2.first_app/routes/userRoutes.js
+++ Mountain/14.NodeJS/2.first_app/routes/userRoutes.js
@@ -3,30 +3,50 @@ const file_path = "./DB/users.json";
 
 module.exports = app => {
 
-  app.put("/user", (req, res) => {
+  app.get("/users", (req, res) => {
+    // jsonfile reading
+    jsonfile.readFile("./DB/users.json", function(err, content) {
+      // send file contents back to sender
+      res.send(content);
+    });
+  });
+  
+  app.get("/user", (req, res) => {
 
     let user;
-    let username = req.body.username;
-    let email    = req.query.email;
+    let email = req.query.email;
 
     jsonfile.readFile(file_path, function(err, content) {
-      for (var i = content.length - 1; i >= 0; i--) {
-        if (content[i].email === req.query.email) {
 
-          console.log("updated user " + req.query.email + " has now username : " + username);
-
+      for (var i = 0 ; i < content.length; i++) {
+        if (content[i].email === email) {
+          console.log("found user" + content[i]);
           user = content[i];
-          user.username = username;
-
         }
       }
+  
+      res.send(user);
+    });
+
+  });
+
+  app.post("/users/new", (req, res) => {
+
+    let {email, username} = req.body;
+    // console.log({email, username});
+
+    jsonfile.readFile(file_path, function(err, content) {
+      
+      content.push({email, username});
+      console.log({email, username}.email + {email, username}.username + " added to DB");
 
       jsonfile.writeFile(file_path, content, function(err) {
         console.log(err);
       });
 
+      res.sendStatus(200);
     });
-    res.send(user);
+
   });
 
   app.delete("/users/destroy", (req, res) => {
@@ -35,13 +55,11 @@ module.exports = app => {
 
     jsonfile.readFile(file_path, function(err, content) {
 
-      for (var i = content.length - 1; i >= 0; i--) {
-
+      for (let i = 0; i < content.length; i++) {
         if (content[i].email === email) {
-          console.log("removing " + content[i].email + "from DB");
+          console.log("removing " + content[i].email + " from DB");
           content.pop(i);
         }
-
       }
 
       jsonfile.writeFile(file_path, content, function(err) {
@@ -50,52 +68,34 @@ module.exports = app => {
 
       res.sendStatus(200);
     });
+
   });
 
-  app.get("/users", (req, res) => {
-    console.log("fetching all users");
-
-    jsonfile.readFile(file_path, function(err, content) {
-      res.send(content);
-    });
-  });
-
-  app.get("/user", (req, res) => {
+  app.put("/user", (req, res) => {
 
     let user;
-    let username = req.query.username;
+    let username = req.body.username;
+    let email = req.query.email;
 
     jsonfile.readFile(file_path, function(err, content) {
-
-      for (var i = content.length - 1; i >= 0; i--) {
-
-        if (content[i].username === username) {
-          console.log("user found");
-          console.log(content[i]);
+      for (let i = 0; i < content.length; i++) {
+        if (content[i].email === email) {
+  
+          console.log("updated user " + email + " has now username : " + username);
+  
           user = content[i];
+          user.username = username;
+  
         }
-
       }
-
-      res.send(user);
-    });
-  });
-
-  app.post("/users/new", (req, res) => {
-
-    let { email, username } = req.body;
-
-    jsonfile.readFile(file_path, function(err, content) {
-
-      content.push({ email, username });
-
-      console.log("added " + email + "to DB");
-
+  
       jsonfile.writeFile(file_path, content, function(err) {
         console.log(err);
       });
-
-      res.sendStatus(200);
+  
     });
+
+    res.send(user);
   });
+
 };
